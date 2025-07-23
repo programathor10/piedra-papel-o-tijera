bc37fd1f41cfe817c0fdd0da18f9f8896188cef1
programathor10 <brunojobs@proton.me>
2025-07-23 04:14:09 -0300

    El backend esta encaminado

---
+++ script.js
@@ -0,0 +1,58 @@
+function getComputerChoice() {
+    const choices = ['piedra', 'papel', 'tijera'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+function getHumanChoice() {
+    let choice = prompt("escribe tu eleccion (piedra, papel o tireja):").toLowerCase();
+    if (!['piedra', 'papel', 'tijera'].includes(choice)) {
+        alert("Invalid choice! Please enter rock, paper, or scissors.");
+        return getHumanChoice();
+    }
+    return choice;
+}
+
+/*console.log(getComputerChoice());*/
+
+let humanScore = 0;
+let computerScore = 0;
+let round = 0;
+
+function playRound() {
+    const humanChoice = getHumanChoice();
+    const computerChoice = getComputerChoice();
+    
+    if (humanChoice === computerChoice) {
+        alert(`It's a tie! You both chose ${humanChoice}.`);
+        round = 0;
+    } else if (
+        (humanChoice === 'piedra' && computerChoice === 'scissors') ||
+        (humanChoice === 'papel' && computerChoice === 'piedra') ||
+        (humanChoice === 'tijera' && computerChoice === 'papel')
+    ) {
+        humanScore++;
+        alert(`Tu ganas! ${humanChoice} vence ${computerChoice}.`);
+    } else {
+        computerScore++;
+        alert(`Tu pierdes! ${computerChoice} vence ${humanChoice}.`);
+    }
+    
+    alert(`Puntaje - Jugador: ${humanScore}, Computer: ${computerScore}`);
+
+    round++;
+    if (round < 1) {
+        playRound();
+    } else {
+        if (humanScore > computerScore) {
+            alert("Felicidades! Has ganado el juego.");
+        } else if (humanScore < computerScore) {
+            alert("Lo siento, has perdido el juego.");
+        } else {
+            alert("El juego termina en empate.");
+        }
+    }    
+
+}
+
+playRound();
